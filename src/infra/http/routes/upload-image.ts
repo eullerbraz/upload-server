cc78cdcf88ff4420ac3d3ee83043d5396508b26d
Euller Braz <euller.braz99@gmail.com>
2025-07-05 10:13:32 -0300

    build: Set up swagger and vitest

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,26 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import { z } from 'zod/v4';
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
-  server.post('/uploads', () => {
-    return 'hello upload';
-  });
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe('Upload already exists'),
+        },
+      },
+    },
+    async (_req, res) => {
+      return res.status(201).send({ uploadId: '' });
+    }
+  );
 };
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,15 +1,17 @@
 import { fastifyCors } from '@fastify/cors';
+import { fastifyMultipart } from '@fastify/multipart';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
 import { fastify } from 'fastify';
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod';
 import { uploadImageRoute } from './routes/upload-image';
 
 const server = fastify();
-
-server.register(fastifyCors, { origin: '*' });
 
 server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
@@ -27,6 +29,19 @@ server.setErrorHandler((error, _req, res) => {
 
   return res.status(500).send({ messsage: 'Internal server error.' });
 });
+
+server.register(fastifyCors, { origin: '*' });
+
+server.register(fastifyMultipart);
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: { title: 'Upload server', version: '1.0.0' },
+  },
+  transform: jsonSchemaTransform,
+});
+
+server.register(fastifySwaggerUi, { routePrefix: '/docs' });
 
 server.register(uploadImageRoute);
 
