e70db4e12aa95e819de172dc2ca18029cec223c8
Euller Braz <euller.braz99@gmail.com>
2025-07-08 16:56:07 -0300

    feat: Add either to error handling

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,4 +1,5 @@
 import { uploadImage } from '@/app/functions/upload-image';
+import { isRight, unwrapEither } from '@/shared/either';
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { z } from 'zod/v4';
 
@@ -10,7 +11,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
         summary: 'Upload an image',
         consumes: ['multipart/form-data'],
         response: {
-          201: z.object({ uploadId: z.string() }),
+          201: z.null().describe('Image uploaded'),
           400: z.object({ message: z.string() }),
           409: z
             .object({ message: z.string() })
@@ -32,13 +33,22 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
       // Ao utilizar o to buffer todo o arquivo seria carregado e aplicação ficaria muito pesada
       // const file = await uploadedFile.toBuffer();
 
-      await uploadImage({
+      const result = await uploadImage({
         fileName: uploadedFile.filename,
         contentType: uploadedFile.mimetype,
         contentStream: uploadedFile.file,
       });
 
-      return res.status(201).send({ uploadId: '' });
+      if (isRight(result)) {
+        return res.status(201).send();
+      }
+
+      const error = unwrapEither(result);
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormat':
+          return res.status(400).send({ message: error.message });
+      }
     }
   );
 };
