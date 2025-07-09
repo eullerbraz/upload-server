0a7a3faaeb4fcfde88570bc5087fb8a452d92436
Euller Braz <euller.braz99@gmail.com>
2025-07-09 09:18:17 -0300

    test: Create upload image unit test

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,10 @@
+import { resolve } from 'node:path';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsConfigPaths],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  },
 });
