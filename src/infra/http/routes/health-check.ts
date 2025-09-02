ee98d6c8ac1dddf4174b67ff08778a71aceb8525
Euller Braz <euller.braz99@gmail.com>
2025-09-02 07:02:24 -0300

    chore: Change health check message to test ecs deploy

--- src/infra/http/routes/health-check.ts
+++ src/infra/http/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 
 export const healthCheckRoute: FastifyPluginAsyncZod = async (server) => {
   server.get('/health', async (_req, res) => {
-    return res.status(200).send({ message: 'Tudo OK!' });
+    return res.status(200).send({ message: 'Tudo OK agora no ECS!' });
   });
 };
